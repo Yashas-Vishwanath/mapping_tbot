360c4b9b1ffa6795a372b4a52a8a2742420bcb19
Yashas-Vishwanath <122467316+Yashas-Vishwanath@users.noreply.github.com>
2023-12-05 12:38:03 +0100

    docker building without error. need to test on nuc

--- .docker/build_image_zsh.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo -e "Building tbot_mapping:lastest image"
-
-DOCKER_BUILDKIT=1 \
-docker build --pull --rm -f ./.docker/Dockerfile \
---build-arg BUILDKIT_INLINE_CACHE=1 \
---target dev \
---tag tbot_mapping:latest .
